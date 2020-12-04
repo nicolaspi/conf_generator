c485febcf4c0fa7ad590f69c79e37803052dea01
nicoco <coco@latulipe>
2020-12-04 12:13:40 +0100

    packaging

--- setup.py
+++ setup.py
@@ -1,10 +1,25 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(name='conf-generator',
-      version='1.0.0',
-      description='Conf-Generator is a tool for specifying and exploring hyper-parameters sets in Machine Learning pipelines defined through configuration files.',
-      packages=find_packages(),
-      setup_requires=['pytest-runner'],
-      tests_require=['pytest'],
-      include_package_data=True
-      )
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="conf-generator",
+    version="1.0.1",
+    author="Nicolas Pinchaud",
+    author_email="nicolas.pinchaud@gmail.com",
+    description="Conf-Generator is a tool for specifying and exploring hyper-parameters sets "
+                "in Machine Learning pipelines defined through configuration files.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nicolaspi/conf_generator",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+          'pyyaml',
+    ],
+)
